d78c8e07e616b8f7d81e20794e6b69cd67a9a987
Jeton Thaçi <jetonthaci@outlook.com>
2019-07-18 12:13:25 +0200

    change open weather apii protocol to https

--- src/fetchData.js
+++ src/fetchData.js
@@ -2,7 +2,7 @@ import { getUnit } from './utils';
 
 const getWeather = async (long, lat) => {
   const unit = getUnit().name;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=${unit}&APPID=4e1f41a544e8138e1382d0bff94cc988`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=${unit}&APPID=4e1f41a544e8138e1382d0bff94cc988`;
   const result = await fetch(url, { mode: 'cors' })
     .then((response) => {
       if (response.status === 200) return response.json();
